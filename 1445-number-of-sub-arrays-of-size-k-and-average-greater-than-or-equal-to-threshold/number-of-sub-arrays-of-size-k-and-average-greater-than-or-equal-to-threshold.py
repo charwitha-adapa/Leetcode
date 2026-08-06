5f1a6fe44af8e75ed0258b123a24d284423e8066
charwitha-adapa <charwitha3597@gmail.com>
2026-08-06 11:00:58 +0530

    Time: 45 ms (60.10%) | Memory: 29.6 MB (98.67%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,18 +1,14 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        n = len(arr)
-        c = 0
-        currsum = 0
-        left = 0
-        for right in range(len(arr)):
-            currsum = currsum + arr[right]
-            if right>=k-1:
-                avg = currsum/k
-                if avg>=threshold:
-                    c = c+1
-                currsum = currsum - arr[left]
-                left = left + 1
-
-        return c
+        first_window = arr[:k]
+        currentsum = sum(first_window)
+        count = 0
+        if currentsum / k >= threshold:
+            count = count + 1 
+        for i in range(k, len(arr)):
+            currentsum = currentsum + arr[i] - arr[i-k]
+            if currentsum/k >= threshold:
+                count = count +1
+        return count
 
         
