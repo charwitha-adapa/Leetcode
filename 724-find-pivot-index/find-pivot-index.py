1da4d3545cb8399e8438a5b709bfce3920b84f66
charwitha-adapa <charwitha3597@gmail.com>
2026-08-31 09:27:14 +0530

    Time: 7 ms (54.30%) | Memory: 20.4 MB (46.68%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,14 +1,17 @@
-import itertools
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
+        p = [0]
         n = len(nums)
-        prefix_sum = list(itertools.accumulate(nums,initial=0))
-        for i in range(len(nums)):
-            l_sum = prefix_sum[i]
-            r_sum = prefix_sum[n] - prefix_sum[i+1]
-            if l_sum==r_sum:
+        s = 0
+        for i in range(n):
+            s = s+nums[i]
+            p.append(s)
+        print(p)
+        r = 0
+        l = 0
+        for i in range(len(p)-1):
+            l = p[i]
+            r = p[n]-p[i+1]
+            if l==r:
                 return i
-                break      
-        else:
-            return -1 
-        
+        return -1        
